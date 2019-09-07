9b43a200008eaa889311e5d35985b8a9cf91742b
V-Kovshov <41215543+V-Kovshov@users.noreply.github.com>
2019-09-07 04:01:28 +0300

    Add files via upload

--- sinergiya/js/custom.js
+++ sinergiya/js/custom.js
@@ -1,4 +1,14 @@
-$('#show-more').click(function() {
-  $('#more').show();
-  $(this).hide();
+$(function () {
+
+	$('.menu-toggle').click(function(){
+		$(this).toggleClass('active')
+		$('.menu').slideToggle(400)
+	})
+
 });
+
+
+// $('#show-more').click(function() {
+//   $('#more').show();
+//   $(this).hide();
+// });
